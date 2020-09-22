9742ebb578210bc0159670968e2643d7a8af3567
Saleem Ali <saleem440011@users.noreply.github.com>
2020-09-22 16:36:05 +0530

    Update Amazon.php

--- Amazon.php
+++ Amazon.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
 
-
-
-public class Amazon(){
-	
-	echo 'Hi';
-	
-}
-
-
+	class Amazon{
+		
+		//Welcome to Amazon Class
+		
+		public function get_title($pageString){
+			
+			$title = "";
+			
+			preg_match('#<title>(.*?)</title>#is',$pageString,$_title);
+			
+			if(isset($_title[1])){
+				return json_encode(array("Title"=>$_title[1]));
+			}
+			else{
+				return json_encode(array("Title"=>$title));
+			}		
+		}
+	}
 ?>
